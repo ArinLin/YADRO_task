efc73e27a6fea34083a9138b92c4f8d91f0dd685
Arina <arinaligit@gmail.com>
2024-03-28 18:40:24 +0300

    add test and multiple lang functionality (eng and rus)

--- main.go
+++ main.go
@@ -2,42 +2,86 @@ package main
 
 import (
 	"bufio"
+	"errors"
+	"flag"
 	"fmt"
-	"github.com/kljensen/snowball"
 	"os"
 	"strings"
+
+	"github.com/kljensen/snowball"
+)
+
+const (
+	rus = "russian"
+	eng = "english"
 )
 
 func main() {
-	// Открываю список стоп-слов
-	file, err := os.Open("stop_words.txt")
+	var sentence string
+	var language string
+	flag.StringVar(&sentence, "s", "", "sentence for stemming")
+	flag.StringVar(&language, "l", "", "stemming language")
+	flag.Parse()
+	if sentence == "" {
+		fmt.Println("Enter sentence for stemming")
+		return
+	}
+	if language == "" {
+		language = "english"
+	}
+
+	stopWordsMap, err := createStopWordsMap(language)
 	if err != nil {
-		panic(err)
+		fmt.Printf("error create stop words map: %s/n", err)
+		return
+	}
+	normalizedSentence, err := normalizeSentence(sentence, language, stopWordsMap)
+	if err != nil {
+		fmt.Printf("error normalize sentence: %s/n", err)
+		return
+	}
+	fmt.Println(normalizedSentence)
+}
+
+// Функция для создания мапы стоп-слов
+func createStopWordsMap(language string) (map[string]struct{}, error) {
+	var fileName string
+	switch language {
+	case rus:
+		fileName = "stop_words_rus.txt"
+	case eng:
+		fileName = "stop_words_eng.txt"
+	default:
+		return nil, errors.New("unavaliable language")
+	}
+	// Открываю список стоп-слов
+	file, err := os.Open(fileName)
+	if err != nil {
+		return nil, err
 	}
 	defer file.Close()
-
 	// Читаю стоп-слова из файла и добавляю их в map
 	stopWordsMap := make(map[string]struct{})
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
 		stopWordsMap[strings.ToLower(scanner.Text())] = struct{}{}
 	}
+	return stopWordsMap, nil
+}
 
-	// Читаю ввод пользователя из консоли
-	fmt.Println("Enter text to normalize:")
-	reader := bufio.NewReader(os.Stdin)
-	input, _ := reader.ReadString('\n')
-
+// Функция для нормализации предложения
+func normalizeSentence(sentence, language string, stopWordsMap map[string]struct{}) (string, error) {
+	var normalizedWords []string
 	// Перебираю слова введенные пользователем, если слово не содержится в стоп-листе, то нормализую и вывожу
-	for _, word := range strings.Fields(input) {
+	for _, word := range strings.Fields(sentence) {
 		toLowerCase := strings.ToLower(word)
-		_, isStopWods := stopWordsMap[toLowerCase]
-		if !isStopWods {
-			stemmed, err := snowball.Stem(toLowerCase, "english", true)
-			if err == nil {
-				fmt.Print(stemmed + " ")
+		if _, isStopWord := stopWordsMap[toLowerCase]; !isStopWord {
+			stemmed, err := snowball.Stem(toLowerCase, language, true)
+			if err != nil {
+				return "", err
 			}
+			normalizedWords = append(normalizedWords, stemmed)
 		}
 	}
-	fmt.Println()
+	return strings.Join(normalizedWords, " "), nil
 }
